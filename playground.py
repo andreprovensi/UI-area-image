cb3892dfb3aa4b7130e54b424905959481e13203
andreprovensi <andreprovensi@gmail.com>
2022-08-04 13:02:10 -0300

    update

--- playground.py
+++ playground.py
@@ -1,3 +1,8 @@
+from tkinter import *
+from tkinter import ttk
+from urllib.request import AbstractDigestAuthHandler
+from PIL import ImageTk, Image
+
 class Point:
 
     def __init__(self, x=0, y=0):
@@ -16,45 +21,67 @@ class Rectangle:
         else:
             pass  
 
-def get_coordinates_on_click(event):
-    x, y = event.x, event.y
-    return x,y
-
-def getsquare(root):
-
-    newrec = Rectangle()
-    root.bind('<Button-1>', get_coordinates_on_click)
+def get_coordinates_for_rectangle(event):
     
-    while len(newrec.points) <4:
-        x,y = get_coordinates_on_click(event)
+    if len(newRec.points)<4:
+        x, y = event.x, event.y
         point = Point(x,y)
-        newrec.get_point(point)
+        newRec.get_point(point)
+        print(x,y)
+    else:
+        print('4 points were already selected')
+        root.unbind('<Button-1>')
 
-    root.unbind('<Button-1>', get_coordinates_on_click)
-    return newrec
-p1 = Point(1,2)
-p2= Point(5,6)
-p3 = Point(-1,-2)
+def create_rectangle(root):
+    
+    if len(newRec.points)>4:
+
+        root.unbind('<Button>')
+    
+    #elif len(newRec.points)<3:
+    else:
+        root.bind('<Button-1>', get_coordinates_for_rectangle)
+    
 
 
-
-# myrec = Rectangle()
-# ponto = Point(1,2)
-
-# myrec.get_point(ponto)
-
-# print(myrec.points[0].y)
-
+def draw_rectangle(event):
+    
+    canvas.create_line(newRec.points[0].x, newRec.points[0].y, newRec.points[3].x, newRec.points[3].y)
+    
+    for i in range(3):
+        canvas.create_line(newRec.points[i].x, newRec.points[i].y, newRec.points[i+1].x, newRec.points[i+1].y)
 
 
-# def get_points(event):
+root = Tk()
 
-#     myRec = Rectangle()
-#     cont=0
-#     while cont<3:
-#         cont+=1
-#         x, y = get_coordinates_on_click(event)
-#         ponto = Point(x,y)
-#         myRec.points.append(ponto)
-        
-#     return myRec
+canvas = Canvas(root, width=600, height=600)
+
+canvas.pack()
+
+picture = Image.open("images/dresden.jpg")
+
+picture_RGB = picture.convert('RGB')
+
+img_tk = ImageTk.PhotoImage(picture)
+
+canvas.create_image(5, 5, anchor=NW, image=img_tk)
+
+newRec = Rectangle()
+
+button = ttk.Button(root,text ='Select Points', command=lambda: root.bind('<Button-1>', get_coordinates_for_rectangle))
+button2 = ttk.Button(root,text ='Draw Rectangle', command=lambda: root.unbind('<Button-1>'))
+#button3 = ttk.Button(root, text='Draw Rectangle')
+
+#button.bind('<Button>', create_rectangle)
+#button.bind('<Return>', lambda: root.unbind('<Button-1>', get_coordinates_for_rectangle), add='+')
+button2.bind('<Button>', draw_rectangle)
+
+button.pack()
+button2.pack()
+#button3.pack()
+
+
+#root.update()
+root.mainloop()
+
+print(f'As coordendas do segundo ponto são {newRec.points[1].x} e {newRec.points[1].y}')
