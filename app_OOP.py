dac3a28689aae27693b64c4a1bfa01310195f6bc
andreprovensi <andreprovensi@gmail.com>
2022-08-11 20:18:37 -0300

    updated app.OOP

--- app_OOP.py
+++ app_OOP.py
@@ -1,13 +1,16 @@
 from tkinter import *
 from tkinter import ttk
 from tkinter.filedialog import askopenfilename
+from PIL import ImageTk, Image
 
 class Point:
+
     def __init__(self, x=0, y=0):
         self.x = x
         self.y = y
 
 class Rectangle:
+
     def __init__(self, points=[]):
         self.points = points
         
@@ -20,13 +23,14 @@ class Rectangle:
 
 class Imagem:
 
-    def __init__(self, file=''):
+    def __init__(self):
 
-        self.file = file
-        self.img = PhotoImage(file=file)
+        self.file = None
+        self.img = None
+        self.src_img = None
 
 class FreeDraw:
-
+    
     def __init__(self, points=[]):
         self.points = points
 
@@ -39,74 +43,84 @@ class FreeDraw:
 class App:
 
     def __init__(self):
-        
-        #Cria a interface gráfica
+
         self.root = Tk()
         self.root.geometry("1000x500")
-        
-        # Instancia o objeto imagem
+
         self.imagem = Imagem()
-        
-        # Instancia o objeto desenho livre
+
         self.freeDraw = FreeDraw()
 
-        # Cria o menu
         self.menubar = Menu(self.root)
-        self.root.config(menu=self.menubar)
+        self.root.config(menu=self.menubar,padx=1,pady=1)
         self.file_menu = Menu(self.menubar,tearoff=False)
         self.menubar.add_cascade(label="File", menu=self.file_menu)
         self.file_menu.add_command(label="Open", command=lambda: self.open_image())
         self.file_menu.add_command(label="Exit", command=lambda: self.root.quit())
-        
-        # Instancia o frame onde a imagem estará contida
+
         self.frame = Frame(self.root)
 
-        # Instancia o canvas onde serão feitos os desenhos
-        self.canvas = Canvas(self.frame, width=self.imagem.img.width(), height=self.imagem.img.height())
-        
-        def __call_free_draw(event):
-            def __free_draw(event):
-                
-                x, y = event.x, event.y
-                ponto = Point(x,y)
-                print(x,y)
-                if len(self.freeDraw.points)==0 or x != self.freeDraw.points[-1].x or y != self.freeDraw.points[-1].y:
-                    self.freeDraw.get_point(ponto)
+        self.canvas = Canvas(self.frame)
 
-            self.freeDraw.points=[]
-            self.root.bind('<Motion>',__free_draw)
+        self.slider = ttk.Scale(self.root,from_=0.1, to=100, orient='horizontal', command = lambda event: self.render_image())
+        self.slider.set(30)
+        
+        def free_draw(event):
+            
             self.root.bind('<Double-Button>', lambda event: self.root.unbind('<Motion>'))
+            x, y = event.x, event.y
+            ponto = Point(x,y)
+            print(x,y)
+            self.freeDraw.get_point(ponto)
+    
+            if len(self.freeDraw.points)>1:
+                self.canvas.create_line(self.freeDraw.points[-2].x, self.freeDraw.points[-2].y, self.freeDraw.points[-1].x, self.freeDraw.points[-1].y)
 
-        self.button_free_draw = ttk.Button(self.root,text ='Free Draw', command=lambda: self.root.bind('<Double-Button>',__call_free_draw) )
-        #self.button_free_draw = ttk.Button(self.root,text ='Free Draw', command=lambda: self.root.bind('<Double-Button>',lambda event: self.root.bind('<Motion>',__free_draw) ))
+        self.button_free_draw = ttk.Button(
+            self.root, text ='Test Spline', 
+            command = lambda: [self.freeDraw.reset_points(),self.root.bind('<Double-Button>', lambda event: self.root.bind('<Motion>',free_draw))]
+        )
 
-        self.button_free_draw.pack(anchor='e',padx=10,pady=10)
+        self.button_free_draw.pack(anchor='e',padx=1,pady=1)
+        self.slider.pack(anchor='e',padx=1,pady=1)
 
-
-        # if len(self.freeDraw.points)>1:
-        #     canvas.create_line(self.freeDraw.points[-2].x, self.freeDraw.points[-2].y, self.freeDraw.points[-1].x, self.freeDraw.points[-1].y)
-
-        
     def open_image(self):
-
-        #self.imagem.img= PhotoImage()
 
         self.imagem.file = askopenfilename(filetypes=[("all files","*"),("Bitmap Files","*.bmp; *.dib"), ("JPEG", "*.jpg; *.jpe; *.jpeg; *.jfif"),("PNG", "*.png"), ("TIFF", "*.tiff; *.tif")])
         
-        self.imagem.img= PhotoImage(file=self.imagem.file)
+        self.imagem.src_img = Image.open(self.imagem.file)
 
-        picture_w, picture_h = self.imagem.img.width(), self.imagem.img.height()
+        self.render_image()
 
-        img_container = self.frame
+    def render_image(self):
 
-        img_container.pack(side=LEFT, padx=50,pady=50)
+        if self.imagem.src_img:
 
-        self.canvas = Canvas(img_container, width=picture_w, height=picture_h)
+            picture=self.imagem.src_img
 
-        self.canvas.create_image(0, 0, anchor=NW, image=self.imagem.img)
+            picture_w, picture_h = picture.size
 
-        self.canvas.pack()
+            picture_w_resized, picture_h_resized = int(picture_w * self.slider.get()/100), int(picture_h * self.slider.get()/100) 
+
+            self.imagem.img = ImageTk.PhotoImage(picture.resize((picture_w_resized, picture_h_resized),resample=Image.LANCZOS))
+
+            self.frame.destroy()
+            
+            self.frame = Frame(self.root,width=picture_w_resized,height=picture_h_resized)
+
+            self.frame.pack(anchor='nw', padx=20,pady=2)
+            
+            self.canvas.destroy()
+                   
+            self.canvas = Canvas(self.frame, width=picture_w_resized, height=picture_h_resized)
+
+            self.canvas.create_image(0, 0, anchor=NW, image=self.imagem.img)
+
+            self.canvas.pack()
+        else:
+            pass
         
+    
 myApp = App()
 
 myApp.root.mainloop()
