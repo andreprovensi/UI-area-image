4473f21875940ae74a6c119869520b9346cce473
André Provensi <81175274+andreprovensi@users.noreply.github.com>
2022-08-25 15:37:41 -0300

    Update app_OOP.py
    comprimentos funcionando

--- app_OOP.py
+++ app_OOP.py
@@ -5,7 +5,7 @@ from PIL import ImageTk, Image
 
 
 class Point:
-    def __init__(self, x=0, y=0):
+    def __init__(self, x=0.0, y=0.0):
         self.x = x
         self.y = y
 
@@ -30,7 +30,7 @@ class Dimension:
     def __init__(self, points=[]):
         self.points = points
         self.length = None
-        self.ratio = None
+        self.px_length = None
     
     def reset_points(self):
         self.points = []
@@ -38,10 +38,19 @@ class Dimension:
     def set_length(self,length):
         self.length = length
 
+class Area:
+    def __init__(self):
+        self.area_px_plan = None
+        self.area_px_proj = None
+        self.area_ratio_px_proj_px_plan = None
+        self.area_m_proj = None
+        self.area_ratio_m_proj_px_proj = None
+
 class FreeDraw:
     def __init__(self, points=[]):
         self.points = points
-        self.area = None
+        self.area_px = None
+        self.area_m = None
 
     def get_point(self,ponto=Point()):   
         self.points.append(ponto)
@@ -98,7 +107,11 @@ class App:
 
         self.freeDraw = FreeDraw()
 
-        self.dimensionRatio = Dimension()
+        self.area = Area()
+
+        self.dimensionRatio_1 = Dimension()
+
+        self.dimensionRatio_2 = Dimension()
         
 
         # Cria o menu para carregar as imagens
@@ -122,8 +135,8 @@ class App:
 
         # Frame e componentes para a definição do tamanho da imagem e da razão mm/pixel
 
-        self.frame_img_prop = Frame(self.root,relief=GROOVE,borderwidth=2)
-        self.frame_img_prop.pack(side=LEFT,fill=BOTH,expand=False)
+        self.frame_img_prop = Frame(self.root,relief=GROOVE,borderwidth=2,width=135)
+        self.frame_img_prop.pack(side=LEFT,fill=Y,expand=False)
 
         self.frame_zoom = Frame(self.frame_img_prop,relief=RIDGE,borderwidth=1)
 
@@ -146,36 +159,40 @@ class App:
 
         # SLIDERS
         self.slider_lable = ttk.Label(self.frame_zoom,text='Zoom',wraplength=90)
-        self.slider = ttk.Scale(self.frame_zoom,from_=1, to=100, orient='horizontal', command = lambda event: self.render_image(),length=105)
+        self.slider = ttk.Scale(self.frame_zoom,from_=1, to=100, orient='horizontal', command = lambda event: self.render_image(),length=125)
         self.slider.set(30)
 
-        # INPUTS, LABEL and LEDS
-        self.dimension_input_lable = ttk.Label(self.frame_input_lable,text='Comprimento conhecido em mm',wraplength=150)
+        ### INPUTS, LABEL and LEDS
+        self.dimension_input_lable = ttk.Label(self.frame_input_lable,text='Comprimentos conhecidos em mm',wraplength=150)
 
+        # LENGTH 1
         self.green_led_figure_1 = ImageTk.PhotoImage(Image.open('small_green_led.jpg'))
         self.red_led_figure_1 = ImageTk.PhotoImage(Image.open('small_red_led.jpg'))
         self.led_1 = ttk.Label(self.frame_input_led_1, image=self.red_led_figure_1 )
-
-        self.green_led_figure_2 = ImageTk.PhotoImage(Image.open('small_red_led.jpg'))
+        self.C1_button = ttk.Button(self.frame_input_led_1, text='C1', width=4, command= lambda: self.C1_button_pressed())
+        
+        # LENGTH 2
+        self.green_led_figure_2 = ImageTk.PhotoImage(Image.open('small_green_led.jpg'))
         self.red_led_figure_2 = ImageTk.PhotoImage(Image.open('small_red_led.jpg'))
         self.led_2 = ttk.Label(self.frame_input_led_2, image=self.red_led_figure_2)
+        self.C2_button = ttk.Button(self.frame_input_led_2, text='C2', width=4, command= lambda: self.C2_button_pressed())
         
         
         self.input_value_1 = StringVar(self.root)
-        self.dimension_input_1 = Entry(self.frame_input_led_1,textvariable=self.input_value_1, bd=3)
+        self.dimension_input_1 = Entry(self.frame_input_led_1,textvariable=self.input_value_1, bd=3,width=15)
         
         self.input_value_2 = StringVar(self.root)
-        self.dimension_input_2 = Entry(self.frame_input_led_2,textvariable=self.input_value_2, bd=3)
+        self.dimension_input_2 = Entry(self.frame_input_led_2,textvariable=self.input_value_2, bd=3,width=15)
         
 
         self.button_set_dimension = ttk.Button(
             self.frame_input_button,text='Selecionar pontos do\ncomprimento',
-            command= lambda: self.button_get_length_pressed()
+            command= lambda: self.C1_button_pressed()
         )
         
         # Action box
-        self.action_box = Message(self.frame_img_prop,text='-Carregue uma imagem\n-Ajuste o zoom\n-Digite o valor do comprimento conhecido\n-Aperte para selecionar os pontos que o definem',bg='light yellow', anchor='n',justify=LEFT)
-        #self.action_box.pack(side=BOTTOM, fill=BOTH,expand=True)
+        # self.action_box = Message(self.frame_img_prop,text='- Carregue uma imagem\n\n- Ajuste o zoom\n\n- Digite os valores dos comprimentos\nconhecidos\n\n- Aperte C1 para definir os pontos\ndo comprimento 1\n\n- Aperte C2 para definir os pontos\ndo comprimento 2\n\n- Quando os dois leds ficarem verdes,\naperte em desenho livre',bg='light yellow', anchor='nw',justify=LEFT, width=120)
+        self.action_box = Message(self.frame_img_prop,text='- Carregue uma imagem\n\n- Ajuste o zoom\n\n- Digite os valores dos comprimentos conhecidos\n\n- Aperte C1 para definir os pontos do comprimento 1\n\n- Aperte C2 para definir os pontos do comprimento 2\n\n- Quando os dois leds ficarem verdes, aperte em desenho livre',bg='light yellow', anchor='nw',justify=LEFT, width=150)
         
     
         #Positioning
@@ -184,11 +201,14 @@ class App:
         self.action_box.pack(side=TOP, fill=BOTH,expand=True)
         self.frame_input_lable.pack(side=TOP)
         self.frame_input_led_1.pack(side = TOP)
-        self.frame_input_led_2.pack(side=TOP)
+        self.frame_input_led_2.pack(side=TOP,pady=5)
         self.frame_input_button.pack(side=TOP)
 
         self.slider_lable.pack(side=LEFT,anchor='w',pady=15)
         self.slider.pack(side=LEFT,anchor='n')
+
+        self.C1_button.pack(side=LEFT,padx=2)
+        self.C2_button.pack(side=LEFT,padx=2)
 
         self.dimension_input_lable.pack(side=TOP,pady=10)
         self.dimension_input_1.pack(side=LEFT)
@@ -196,8 +216,8 @@ class App:
 
         self.led_1.pack(side=RIGHT,anchor='ne')
         self.led_2.pack(side=RIGHT,anchor='ne')
-        self.button_set_dimension.pack(side=LEFT,pady=10)
-        #self.green_led.
+        #self.button_set_dimension.pack(side=LEFT,pady=10)
+    
         # Canvas
         self.canvas = Canvas(self.frame)
 
@@ -212,7 +232,7 @@ class App:
         self.slider.pack(side=TOP,padx=1,pady=15)
 
     def check_free_draw(self):
-        if self.dimensionRatio.ratio:
+        if self.area.area_ratio_m_proj_px_proj:
             self.render_image()
             self.action_box.config(text='Clique duas vezes para começar o desenho e, chegando perto do final do desenho, clique novamente duas vezes')
             self.freeDraw.reset_points()
@@ -222,7 +242,7 @@ class App:
             messagebox.showerror('','Você precisa definir o comprimento conhecido')
     
     def free_draw(self,event):
-        if self.dimensionRatio.ratio:
+        if self.area.area_ratio_m_proj_px_proj:
             self.root.bind('<Double-Button>', lambda event: [self.root.unbind('<Motion>'),self.freeDraw.closing_points_free_draw(),self.close_free_draw(),self.calcula_area_geom()]) 
             x, y = event.x, event.y
             ponto = Point(x,y)
@@ -232,37 +252,103 @@ class App:
                 self.canvas.create_line(self.freeDraw.points[-2].x, self.freeDraw.points[-2].y, self.freeDraw.points[-1].x, self.freeDraw.points[-1].y)
         else:
             self.root.unbind('<Motion>')
-            messagebox.showerror('','Você precisa definir o comprimento conhecido')
+            messagebox.showerror('','Você precisa definir os comprimentos conhecidos')
 
-    def button_get_length_pressed(self):
+    
+    def set_proj_plan_ratio(self):
+        P1 = self.dimensionRatio_1.points[0]
+        P2 = self.dimensionRatio_1.points[1]
+        P3 = self.dimensionRatio_2.points[0]
+        P4 = self.dimensionRatio_2.points[1]
+
+        delta_x_1 = P2.x - P1.x
+        delta_y_1 = P2.y - P1.y
+        angular_coeff_1 = delta_y_1 / delta_x_1
+        linear_coeff_1 = P1.y - angular_coeff_1 * P1.x
+
+        delta_x_2 = P4.x - P3.x
+        delta_y_2 = P4.y - P3.y
+        angular_coeff_2 = delta_y_2 / delta_x_2
+        linear_coeff_2 = P3.y - angular_coeff_2 * P3.x
+
+        P0_x = (linear_coeff_2 - linear_coeff_1) / (angular_coeff_1 - angular_coeff_2)
+        P0_y = angular_coeff_1 * P0_x + linear_coeff_1
+
+        P0 = Point(P0_x, P0_y)
+
+        # Definir os pontos com máximo delta_x e delta_y entre P0 e os outros pontos 
+                
+        vetor_1 = Point(max(abs(P1.x - P0.x), abs(P2.x - P0.x)), max(abs(P1.y - P0.y), abs(P2.y - P0.y)))
+
+        vetor_2 = Point(max(abs(P3.x - P0.x), abs(P3.x - P0.x)), max(abs(P4.y - P0.y), abs(P4.y - P0.y)))
+
+        self.area.area_px_plan = ((vetor_1.x*vetor_2.y) ** 2 + (vetor_2.x*vetor_1.y) ** 2) ** 0.5
+
+        self.area.area_px_proj = (vetor_1.x**2 + vetor_1.y**2)**0.5 * (vetor_2.x**2 + vetor_2.y**2)**0.5
+
+        self.area.area_ratio_px_proj_px_plan = self.area.area_px_proj / self.area.area_px_plan
+
+        length_1 = self.dimensionRatio_1.length * (vetor_1.x**2 + vetor_1.y**2)**0.5 / (delta_x_1**2 + delta_y_1**2)**0.5
+
+        length_2 = self.dimensionRatio_2.length * (vetor_2.x**2 + vetor_2.y**2)**0.5 / (delta_x_2**2 + delta_y_2**2)**0.5
+
+        self.area.area_m_proj = length_1 * length_2 
+
+        self.area.area_ratio_m_proj_px_proj = length_1 * length_2 / self.area.area_px_proj
+
+
+    def C1_button_pressed(self):
         if self.input_value_1.get():
             self.render_image() #Caso já tenha algo desenhado, uma nova imagem é renderizada ao apertar o botão
-            self.action_box.config(text='-Selecione os pontos do comprimento conhecido\n-Clique em Desenho Livre')
-            self.dimensionRatio.reset_points()
-            self.dimensionRatio.set_length(float(self.input_value_1.get()))
-            self.root.bind('<Button-1>', lambda event: self.get_points_for_dimension(event))
+            self.action_box.config(text='- Selecione os pontos do comprimento conhecido')
+            self.dimensionRatio_1.reset_points()
+            self.dimensionRatio_1.set_length(float(self.input_value_1.get()))
+            self.root.bind('<Button-1>', lambda event: self.get_C1_points(event))
             
         else:
             messagebox.showerror('','Você precisa digitar o comprimento conhecido')
             
         
-    def get_points_for_dimension(self, event):   
+    def get_C1_points(self, event):   
         ponto=Point(event.x,event.y)
         
-        if len(self.dimensionRatio.points)<=1:
-            self.dimensionRatio.points.append(ponto)
+        if len(self.dimensionRatio_1.points)<=1:
+            self.dimensionRatio_1.points.append(ponto)
             self.canvas.create_oval((event.x,event.y,event.x,event.y),fill='black',width=5)
-            
-        elif len(self.dimensionRatio.points) == 2:
-            delta_x = abs(self.dimensionRatio.points[1].x - self.dimensionRatio.points[0].x)
-            delta_y = abs(self.dimensionRatio.points[1].y - self.dimensionRatio.points[0].y)
-            dist_px = (delta_x**2 + delta_y**2)**0.5
-            self.dimensionRatio.ratio = self.dimensionRatio.length/dist_px
-            self.render_image()
-            self.root.unbind('<Button-1>')
         
-        if len(self.dimensionRatio.points) == 2:
+        if len(self.dimensionRatio_1.points) == 2:
             self.led_1.config(image=self.green_led_figure_1)
+            self.root.unbind('<Button-1>')
+
+        if len(self.dimensionRatio_1.points) == 2 and len(self.dimensionRatio_2.points) == 2:
+            self.set_proj_plan_ratio()
+
+    def C2_button_pressed(self):
+        if self.input_value_2.get():
+            self.render_image() #Caso já tenha algo desenhado, uma nova imagem é renderizada ao apertar o botão
+            self.action_box.config(text='- Selecione os pontos do comprimento conhecido')
+            self.dimensionRatio_2.reset_points()
+            self.dimensionRatio_2.set_length(float(self.input_value_2.get()))
+            self.root.bind('<Button-1>', lambda event: self.get_C2_points(event))
+            
+        else:
+            messagebox.showerror('','Você precisa digitar o comprimento conhecido')
+            
+        
+    def get_C2_points(self, event):   
+        ponto=Point(event.x,event.y)
+        
+        if len(self.dimensionRatio_2.points)<=1:
+            self.dimensionRatio_2.points.append(ponto)
+            self.canvas.create_oval((event.x,event.y,event.x,event.y),fill='black',width=5)
+   
+        
+        if len(self.dimensionRatio_2.points) == 2:
+            self.led_2.config(image=self.green_led_figure_2)
+            self.root.unbind('<Button-1>')
+
+        if len(self.dimensionRatio_1.points) == 2 and len(self.dimensionRatio_2.points) == 2:
+            self.set_proj_plan_ratio()
 
 
     def open_image(self):
@@ -305,32 +391,35 @@ class App:
         self.canvas.create_line(points_list)
 
     def calcula_area_geom(self):
-        if len(self.freeDraw.points)>2 and self.dimensionRatio.ratio:
+        if len(self.freeDraw.points)>2 and self.area.area_ratio_m_proj_px_proj:
             
             unique_list = [self.freeDraw.points[0]]
             #unique_list = self.freeDraw.points
-            areas=[]
+            areas_px=[]
 
             for i, ponto in enumerate(self.freeDraw.points[1:]):
                 if self.freeDraw.points[i].x != self.freeDraw.points[i-1].x or self.freeDraw.points[i].y != self.freeDraw.points[i-1].y:
                     unique_list.append(ponto)
                 else:
                     pass
-                
+
             for i, ponto in enumerate(unique_list[0:-1]):
                 delta_x_px = unique_list[i+1].x - unique_list[i].x
                 delta_y_px = unique_list[i+1].y - unique_list[i].y
-                delta_x_m = delta_x_px*self.dimensionRatio.ratio
-                delta_y_m = abs(delta_y_px*self.dimensionRatio.ratio)
-                y_1_m = min(unique_list[i].y,unique_list[i+1].y)*self.dimensionRatio.ratio
-                area = delta_x_m * (y_1_m + delta_y_m/2)
+                #delta_x_m = delta_x_px*self.area.area_ratio_m_proj_px_proj
+                #delta_y_m = abs(delta_y_px*self.area.area_ratio_m_proj_px_proj)
+                #y_1_m = min(unique_list[i].y,unique_list[i+1].y)*self.area.area_ratio_m_proj_px_proj
+                y_1_px = min(unique_list[i].y,unique_list[i+1].y)
+                area = delta_x_px * (y_1_px + delta_y_px/2)
 
-                areas.append(area)
+                areas_px.append(area)
             
+            self.freeDraw.area_px = abs(sum(tuple(areas_px)))
             
-
-            self.freeDraw.area = abs(sum(tuple(areas)))
-            self.action_box.config(text=f'A área da figura é {abs(sum(tuple(areas))):.2f} mm²')
+            area_meters = self.freeDraw.area_px * self.area.area_ratio_m_proj_px_proj
+            self.freeDraw.area_m = area_meters
+            
+            self.action_box.config(text=f'A área da figura é {area_meters:.2f} mm²')
             
                  
 
